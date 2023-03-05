2e755f3b5985f3d9acb54ae6b632f6d3d70e2fa5
Dhaval-ACE <mihirsangani45@gmail.com>
2023-03-05 15:59:19 +0530

    Update calculation.module.js

--- routes/api/calculation/calculation.module.js
+++ routes/api/calculation/calculation.module.js
@@ -54,44 +54,8 @@ const calculateSIPPerformanceModule = async (req) => {
     }
 }
 
-const calculateSpiModule = async (req) => {
-    var monthlySipAmount = req.body.monthly_sip_amount
-    var expectedReturn = req.body.expected_return
-    var timePeriod = req.body.time_period
-
-    if (monthlySipAmount == undefined || monthlySipAmount == "" || monthlySipAmount == null || expectedReturn == undefined || expectedReturn == null || expectedReturn == "" || timePeriod == undefined || timePeriod == "" || timePeriod == null) {
-        return {
-            status: false,
-            error: constant.requestMessages.ERR_INVALID_BODY
-        }
-    }
-
-    async function calculateSIP(monthlyInvestment, expectedReturn, timePeriod, inflationRate) {
-        var exp = expectedReturn / 100
-        var n = timePeriod * 12
-        var i = exp / 12
-        var oneI = 1 + i
-        var onePower = Math.pow(oneI, n)
-        var onePowerSubtrect = onePower - 1
-        var middle = onePowerSubtrect / i
-        // var M = P * ({[1 + i]n - 1} / i) * (1 + i)
-        var total_value = Math.round(monthlyInvestment * middle * oneI)
-        var total_investment = monthlyInvestment * n
-        var returns = total_value - total_investment
-        return {
-            total_value,
-            returns,
-            total_investment
-        }
-    }
-
-    const result = calculateSIP(monthlySipAmount, expectedReturn, timePeriod)
-    return result
-}
-
 module.exports = {
-    calculateSIPPerformanceModule: calculateSIPPerformanceModule,
-    calculateSpiModule: calculateSpiModule
+    calculateSIPPerformanceModule: calculateSIPPerformanceModule
 }
 
 async function filterData(SIPStartDateNavData, schemeCodeData, formatedDate, skipDay, searchDate, flag) {
